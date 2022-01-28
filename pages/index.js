65688b313fbc8f83743f4fcae4fa6cde393b9f91
Rachel <rachelreillydev@gmail.com>
2022-01-28 15:58:03 -0600

    creates and styles Button

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import {useState} from 'react'
 import { DotBox } from '../components/shapes/DotBox'
 import {Rgb, Hsla} from '../classes'
 import {INCREMENT} from '../constants'
+import { Button } from '../components/Button'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -28,7 +29,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1>Generative art playground</h1>
-        <button onClick={handleChangeColor}>YOOOO</button>
+        <Button content="Click to change color" onClick={handleChangeColor}/>
         <div >
         {arr.map((_, index)=> <DotBox key={index} prop='h' dotProp='l' increment={index * INCREMENT} color={color}/>
           )}
